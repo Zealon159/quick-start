ae6a9d914531dce8f2caf94b76048d7f840e3bd3
Zealon <997717740@qq.com>
2018-06-21 12:58:18 +0800

    commit projects.

---
+++ quick-thymeleaf/src/main/java/cn/zealon/QuickThymeleafApplication.java
@@ -0,0 +1,12 @@
+package cn.zealon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickThymeleafApplication.class, args);
+    }
+}
