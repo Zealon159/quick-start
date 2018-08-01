523208e75dca3aaa1450f4d2ca3aeee2f0a78bc3
Zealon159 <zealon@126.com>
2018-08-01 22:33:58 +0800

    update.

--- quick-springmvc/src/main/java/cn/zealon/mvc/config/DateConvert.java
+++ quick-springmvc/src/main/java/cn/zealon/mvc/config/DateConvert.java
@@ -1,32 +1,24 @@
 package cn.zealon.mvc.config;
 
-import org.springframework.format.Formatter;
-
+import org.springframework.core.convert.converter.Converter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 /**
  * @auther: Zealon
  * @Date: 2018-07-12 17:27
  */
-public class DateConvert  implements Formatter<Date> {
+public class DateConvert implements Converter<String, Date> {
+
     @Override
-    public Date parse(String s, Locale locale) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-        Date d = null;
+    public Date convert(String stringDate) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         try {
-            d = simpleDateFormat.parse(s);
+            return simpleDateFormat.parse(stringDate);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return d;
-    }
-
-    @Override
-    public String print(Date date, Locale locale) {
         return null;
     }
-
 }
