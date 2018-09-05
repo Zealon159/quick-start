6b8fab698e07f9f73ea28dbb00edc3e1c8d9d5a9
Zealon159 <zealon@126.com>
2018-09-05 23:14:59 +0800

    增加指定用户推送消息.

--- quick-websocket/src/main/java/cn/zealon/websocket/WebController.java
+++ quick-websocket/src/main/java/cn/zealon/websocket/WebController.java
@@ -1,7 +1,12 @@
 package cn.zealon.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 /**
  * @auther: Zealon
@@ -10,8 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
+    @Autowired
+    private WebSocketTest webSocketTest;
+
     @RequestMapping("index")
     public String index(){
         return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("send/{uid}/{msg}")
+    public String sendMsg(@PathVariable("uid") String uid,@PathVariable("msg") String msg){
+
+        webSocketTest.sendToUser(msg+"|"+uid);
+        return "ok?";
     }
 }
--- quick-websocket/src/main/java/cn/zealon/websocket/WebSocketTest.java
+++ quick-websocket/src/main/java/cn/zealon/websocket/WebSocketTest.java
@@ -3,36 +3,47 @@ package cn.zealon.websocket;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 /**
  * @auther: Zealon
  * @Date: 2018-09-05 16:38
  */
-@ServerEndpoint(value = "/websocket")
+@ServerEndpoint(value = "/websocket/{userno}")
 @Component
 public class WebSocketTest {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
 
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
-    private static CopyOnWriteArraySet<WebSocketTest> webSocketSet = new CopyOnWriteArraySet<WebSocketTest>();
+    public static ConcurrentHashMap<String, WebSocketTest> webSocketSet = new ConcurrentHashMap<String, WebSocketTest>();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
+
+    //当前发消息的人员编号
+    private String userno = "";
 
     /**
      * 连接建立成功调用的方法
      * @param session  可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
     @OnOpen
-    public void onOpen(Session session){
+    public void onOpen(@PathParam(value = "userno") String param, Session session){
         this.session = session;
-        webSocketSet.add(this);     //加入set中
+        userno = param;
+        webSocketSet.put(userno,this);     //加入set中
         addOnlineCount();           //在线数加1
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+        System.out.println(session.getUserPrincipal());
+        System.out.println(session.getUserProperties());
     }
 
     /**
@@ -40,9 +51,11 @@ public class WebSocketTest {
      */
     @OnClose
     public void onClose(){
-        webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
-        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+        if (!userno.equals("")) {
+            webSocketSet.remove(userno);  //从set中删除
+            subOnlineCount();           //在线数减1
+            System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+        }
     }
 
     /**
@@ -54,14 +67,47 @@ public class WebSocketTest {
     public void onMessage(String message, Session session) {
         System.out.println("来自客户端的消息:" + message);
         //群发消息
-        for(WebSocketTest item: webSocketSet){
+        /*for(WebSocketTest item: webSocketSet){
             try {
                 item.sendMessage(message);
             } catch (IOException e) {
                 e.printStackTrace();
                 continue;
             }
+        }*/
+
+        sendToUser(message);
+    }
+
+    /**
+     * 给指定的人发送消息
+     * @param message
+     */
+    public void sendToUser(String message) {
+        String sendUserno = message.split("[|]")[1];
+        String sendMessage = message.split("[|]")[0];
+        String now = getNowTime();
+        try {
+            if (webSocketSet.get(sendUserno) != null) {
+                webSocketSet.get(sendUserno).sendMessage(now + "用户" + userno + "发来消息：" + " <br/> " + sendMessage);
+            } else {
+                System.out.println("当前用户不在线");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @return
+     */
+    private String getNowTime() {
+        Date date = new Date();
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = format.format(date);
+        return time;
     }
 
     /**
@@ -81,6 +127,7 @@ public class WebSocketTest {
      * @throws IOException
      */
     public void sendMessage(String message) throws IOException{
+        System.out.println("send:"+message);
         this.session.getBasicRemote().sendText(message);
         //this.session.getAsyncRemote().sendText(message);
     }
