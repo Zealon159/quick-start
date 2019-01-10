6ca9fc2d0ff47e4251b21c699cfa95322cb02393
Zealon <zealon@126.com>
2019-01-10 17:33:04 +0800

    elastic init.

---
+++ quick-elastic-search/src/main/java/cn/zealon/QuickElasticSearchApplication.java
@@ -0,0 +1,14 @@
+package cn.zealon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickElasticSearchApplication.class, args);
+    }
+
+}
+
