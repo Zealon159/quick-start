19750b61157b4aa001c598f0df75d1518c5d37c1
Zealon <zealon@126.com>
2019-10-14 18:59:31 +0800

    消费端增加，ack机制.

--- quick-rabbitmq-sub/src/main/java/cn/zealon/rabbitsub/sub/Subscribe.java
+++ quick-rabbitmq-sub/src/main/java/cn/zealon/rabbitsub/sub/Subscribe.java
@@ -1,9 +1,14 @@
 package cn.zealon.rabbitsub.sub;
 
 import cn.zealon.rabbitsub.config.AMQPConfig;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Delivery;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * 消息订阅者
@@ -13,9 +18,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class Subscribe {
 
-    @RabbitListener(queues = AMQPConfig.BOOK_STATUS_QUEUE, concurrency = "1-5")
-    public void processMessage(Message message) {
+    @RabbitListener(queues = AMQPConfig.BOOK_STATUS_QUEUE, concurrency = "1-5" )
+    public void processMessage(Message message,Channel channel) {
         String body = new String(message.getBody());
-        System.out.println("sub2:"+body);
+
+        // 确认消费
+        Long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        try {
+            System.out.println("sub2:"+body+"; deliveryTag:"+deliveryTag);
+            channel.basicAck(deliveryTag,false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
